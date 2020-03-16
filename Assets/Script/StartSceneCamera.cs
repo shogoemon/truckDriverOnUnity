210e79ed51703d3684a4aa82193b40182a404190
shibuya <shibuya.shogo@icloud.com>
2020-03-17 03:14:37 +0900

    画面サイズ変更、アプリアイコン変更

---
+++ Assets/Script/StartSceneCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneCamera : MonoBehaviour
+{
+
+    //最初に作った画面のwidth
+    float defaultWidth = 2436.0f;
+
+    //最初に作った画面のheight
+    float defaultHeight = 1135.0f;
+
+    void Start()
+    {
+               //camera.mainを変数に格納
+               Camera mainCamera = Camera.main;
+
+               //最初に作った画面のアスペクト比
+               float defaultAspect = defaultWidth / defaultHeight;
+
+               //実際の画面のアスペクト比
+               float actualAspect = (float)Screen.width / (float)Screen.height;
+
+               //実機とunity画面の比率
+               float ratio = actualAspect / defaultAspect;
+               Debug.Log(ratio);
+
+               //サイズ調整
+               mainCamera.orthographicSize /= ratio;
+    }
+}
